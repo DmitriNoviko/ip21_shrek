3d151b1a8c11500db8994653e7fcc2504be0f380
novikov <enemy-nsk@mail.ru>
2023-09-07 19:23:50 +0700

    python

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 Y = input("ТЫ ХТО?")
 print (Y+"chikibomboni")
-x = input("1 chislo vadi padla")
-y = input("a 2???????")
+
+x = int(input("1 chislo vadi padla"))
+y = int(input("a 2???????"))
 z = x + y 
-print("popei govna:" + z)
+print("popei govna:", z)
+
+cel = float(input("Напиши темпуру шаурмыыыы: "))
+
+far = (cel * 1.8) + 32
+
+print(str(cel )+ " Цельсий равно " + str(far )+
+	" Фаренгейт братиш")
+
+
